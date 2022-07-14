1fc2c6d9d8831842bb3805958a1de77437208971
Belem Zhang <belem.zhang@intel.com>
2022-07-14 13:34:03 +0800

    debug uph

--- src/script/pages/sample-uph.ts
+++ src/script/pages/sample-uph.ts
@@ -29,9 +29,11 @@ export class SampleUPH extends LitElement {
       let geocodes = json.geocodes;
       console.log(geocodes);
       let location = geocodes[0].location;
-      this._geolocation.innerHTML = location;
+      this._geolocation.innerHTML = '经纬度：' + location;
       const response = await fetch(`https://restapi.amap.com/v3/staticmap?location=${location}&zoom=10&size=375*250&markers=mid,,A:${location}&key=39a5a5f5239a28b739e6a79381afb97e`);
-      this._map.innerHTML = `<img src="${response}">`;
+      let blob = response.blob();
+      const imageObjectURL = URL.createObjectURL(blob);
+      this._map.innerHTML = `<img src="${imageObjectURL}">`;
     } else {
       this._msg.innerHTML = `无 web+pwadev://* 调用`;
     }
